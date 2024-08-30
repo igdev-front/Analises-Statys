eb8693617ea93f7e79d217e6172196dd2df06415
igdev-front <igdevfront@gmail.com>
2024-08-29 23:28:24 -0300

    tv

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navList = document.querySelector('.nav-list');
+
+    menuToggle.addEventListener('click', function () {
+        navList.classList.toggle('active');
+    });
+});
